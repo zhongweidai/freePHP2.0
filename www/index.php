fb9b33683df5a6c51f914dec4a0f4e10b554914d
root <root@localhost.dzw>
2016-03-25 16:23:54 +0800

    add

---
+++ www/index.php
@@ -0,0 +1,11 @@
+<?php
+ob_start();
+//框架路径
+    define('FREE_PATH', dirname(dirname(__FILE__)).DIRECTORY_SEPARATOR);
+	define('FREE_DEBUG',true);
+	define('FREE_RUNTIME',false);//是否开启运行缓存 默认开启
+	include FREE_PATH . '/free/Free.php';
+    $loader = new FreeKernel();
+    $loader->run();
+
+?>
